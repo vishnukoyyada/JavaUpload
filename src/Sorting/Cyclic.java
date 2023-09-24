cca0de5e78d1ad9a3e5ebde5303a41854e12e12b
vishnukoyyad03 <vishnukoyyad03@gmail.com>
2023-09-24 10:59:46 +0530

    Added cycle  sort

---
+++ src/Sorting/Cyclic.java
@@ -0,0 +1,36 @@
+/*
+ * Author:kvishnuv
+ * Title: Cyclic sort
+ * Time complexity O(n)
+ * take a value and put it in a correct position if it is already at correct position then move the pointer or else keep changing
+ * I think this algoritham is limited because  i think all elements should be from 1 to n  like continouous 
+ */
+
+package Sorting;
+
+import java.util.*;
+
+public class Cyclic {
+    public static void main(String[] args) {
+        int[] arr = {3,4,1,2,5};
+        cycle(arr);
+        System.out.println(Arrays.toString(arr));
+
+        
+    }
+    public static void swap(int[] arr,int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+    public static void cycle(int[] arr){
+        for(int i =0;i<arr.length;i++){
+            if(arr[i]-1 != i){
+                swap(arr,i,arr[i]-1);
+            }else{
+                continue;
+            }
+
+        }
+    }
+}
